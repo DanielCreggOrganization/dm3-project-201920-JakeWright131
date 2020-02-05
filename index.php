ca3a7108dd7043dbf6d07922d6d85e0a39214773
JakeWright131 <g00357576@gmit.ie>
2020-02-05 15:55:55 +0000

    made changes

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <html>
     <head>
+        <link rel="stylesheet" href="styles.css">
+        
     </head>
     <body>
-            <h1>My website</h1>
-            <p id=demo> Hello World </p>
-    <body>
-</html> 
+            <h1>My Website</h1>
+            <p id=demo>Hello World!</p>
+            <script type="text/javascript" src="myscripts.js"></script>
+            <button onclick="myFunction();">Click Me to turn text blue</button>
+            
+    </body>
+</html>
